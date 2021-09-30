02613d3702d13752ac999174539600c88dc0aa20
issiscv <issiscv46@gmail.com>
2021-09-30 19:37:03 +0900

    add custom logout logic

--- src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -1,7 +1,14 @@
 package io.security.corespringsecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class LoginController {
@@ -9,5 +16,19 @@ public class LoginController {
     @GetMapping("/login")
     public String login() {
         return "user/login/login";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+
+
+
+        return "redirect:/login";
     }
 }
