072c2abf6be989639d91a984a3c571ff0acb043b
issiscv <issiscv46@gmail.com>
2021-09-30 19:19:11 +0900

    add custom login page

---
+++ src/main/java/io/security/corespringsecurity/controller/login/LoginController.java
@@ -0,0 +1,13 @@
+package io.security.corespringsecurity.controller.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "user/login/login";
+    }
+}
--- src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
+++ src/main/java/io/security/corespringsecurity/security/configs/SecurityConfig.java
@@ -47,7 +47,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/config").hasRole("ADMIN")
                 .anyRequest().authenticated()
         .and()
-                .formLogin();
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/login_proc")
+                .defaultSuccessUrl("/")
+                .permitAll()
+        ;
     }
 
 
